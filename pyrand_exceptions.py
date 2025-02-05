e6df15f23e834a1e7c601fbeefff01e494e35ef7
KierenWilliamson <kierenwilliamson129@gmail.com>
2025-02-04 23:14:16 -0500

    moved files

--- pyrand_exceptions.py
+++
@@ -1,7 +0,0 @@
-class UnsetButtonError(Exception):
-    # raised when one or more radio buttons are left un-toggled
-    pass
-
-class LengthError(Exception):
-    # raised when the length is longer than the maximum number possible characters
-    pass
